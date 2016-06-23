70a305839f5db7082e01b67ce3a429a5c7f2529a
Karnitskaya Kristina <kkarnitskaya@marathonbet.ru>
2016-06-23 14:01:12 +0300

    Get current weather

--- scripts/communication.js
+++ scripts/communication.js
@@ -1,19 +1,21 @@
-var answers = [
-  "I could tell you, but then I'd have to kill you",
-  "Answering that would be a matter of national security",
-  "You can't possibly compare them!",
-  "Both hold a special place in my heart"
-];
+// Minsk id=625144
+var queries = ['Wheather', 'What the weather today?'];
 
 module.exports = function(robot) {
-  return robot.respond(/(which|who) is (better|worse)\?* (.*) or (.*?)\??$/i, function(msg) {
-    var choosen_response;
-    choosen_response = msg.random([1, 2, 3, 4, 5]);
-    if (choosen_response >= 3) {
-      return msg.send(msg.random(answers));
-    } else {
-      return msg.send("Clearly " + msg.match[choosen_response + 2] + " is " + msg.match[2]);
-    }
+  queries.forEach(function(query) {
+    robot.hear(new RegExp(query, 'i'), function (response) {
+      var currentWeather;
+      robot.http("http://api.openweathermap.org/data/2.5/forecast/city?id=625144&APPID=62cb294f506741358b20565888f4a2e2").get()(function(err, res, body) {
+        if (err) {
+          console.log('error');
+        }
+        else {
+          currentWeather = body;
+          console.log(body.city);
+        }
+        response.reply(currentWeather);
+      });
+    });
   });
 };
 
