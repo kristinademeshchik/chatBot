0e197390030b625c07924da271dd1980278d799f
Karnitskaya Kristina <kkarnitskaya@marathonbet.ru>
2016-06-23 17:12:55 +0300

    Set hubot talk by schedule

--- scripts/communication.js
+++ scripts/communication.js
@@ -1,21 +1,39 @@
-// Minsk id=625144
-var queries = ['Wheather', 'What the weather today?'];
+/*
+ Description:
+ In order to get ID number of any city use the following link:
+ http://bulk.openweathermap.org/sample/
 
-module.exports = function(robot) {
-  queries.forEach(function(query) {
-    robot.hear(new RegExp(query, 'i'), function (response) {
-      var currentWeather;
-      robot.http("http://api.openweathermap.org/data/2.5/forecast/city?id=625144&APPID=62cb294f506741358b20565888f4a2e2").get()(function(err, res, body) {
+ Minsk id = 625144
+
+ Dependencies:
+  * node-schedule
+
+ Configuration:
+ CITY_ID
+ */
+
+var cron = require('node-schedule');
+
+module.exports = function (robot) {
+
+  robot.hear(new RegExp('Send me the weather, pls', 'i'), function (response) {
+    var currentWeather,
+      url = 'http://api.openweathermap.org/data/2.5/forecast/city?id=' + process.env.CITY_ID + '&units=metric&APPID=62cb294f506741358b20565888f4a2e2';
+
+    cron.scheduleJob({hour: 17, minute: 13, dayOfWeek: [1,2,3,4,5,6,7]}, function(){
+      robot.http(url).get()(function (err, res, body) {
         if (err) {
           console.log('error');
         }
         else {
-          currentWeather = body;
-          console.log(body.city);
+          currentWeather = JSON.parse(body);
         }
-        response.reply(currentWeather);
+        response.send('Current temperature is ' +  currentWeather.list[0].main.temp);
       });
     });
+
+    response.send('No problem ;)');
   });
+
 };
 
